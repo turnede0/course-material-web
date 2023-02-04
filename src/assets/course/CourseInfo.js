6f8a0f6e03d2410cb4e2cde66233a3ac81a056ec
turnede0 <79206052+turnede0@users.noreply.github.com>
2023-02-04 17:54:32 +0800

    Updated course info
    Course info test and Chinese wording test

--- src/assets/course/CourseInfo.js
+++ src/assets/course/CourseInfo.js
@@ -1,37 +1,57 @@
 export const CourseInfo = [
   {
-    category: "hardware",
+    category: "Hardware",
     categoryDescription:
-      "Hardware is best described as any physical component of a computer system containing a circuit board, ICs, or other electronics. A perfect example of hardware is the screen on which you are viewing this page. Whether it be a monitor, tablet, or smartphone, it is hardware.",
+      "無論是顯示器，平板電腦還是智能手機，都是硬件。 我們社會所有現有技術的皆由硬件支撐。 通過物理和電子知識的各種組合，我們可以創造出從日常生活、科學研究、能量采集到硬件加速等任何方面來幫助人類的設備",
     courses: [
       {
-        courseName: "Intro to PCB",
-        courseID: "pcb001",
+        courseName: "印刷電路板入門",
+        courseID: "PCB001",
         description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum molestiae delectus culpa hic assumenda, voluptate reprehenderit dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit, eveniet ex deserunt fuga?",
-        category: "hardware",
+          "從零開始的印刷電路板設計入門之旅！手把手教程學習如何設計原理圖及佈板。制造属於您的第一塊電板！",
+        category: "Hardware",
         image: "",
         downloadUrl:
           "https://stemturnede.sharepoint.com/sites/info.public.turned-e/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2Finfo%2Epublic%2Eturned-e%2FShared%20Documents%2FCourse&p=true&ga=1",
         resources: [
           {
-            topic: "Propositional logic",
+            topic: "Lesson01 認識EasyEDA",
             downloadUrl:
-              "https://stemturnede.sharepoint.com/sites/info.public.turned-e/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2Finfo%2Epublic%2Eturned-e%2FShared%20Documents%2FCourse&p=true&ga=1",
+              "https://stemturnede.sharepoint.com/:b:/g/EUX6RkqsbTNJh0jcvj3IJLMBF-cIp_6MVa3ZavJYlk-1Ag?e=8fG1I0",
           },
           {
-            topic: "Predicate logic",
+            topic: "Lesson02 電路設計101"
             downloadUrl:
-              "https://stemturnede.sharepoint.com/sites/info.public.turned-e/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2Finfo%2Epublic%2Eturned-e%2FShared%20Documents%2FCourse&p=true&ga=1",
+              "https://stemturnede.sharepoint.com/:f:/g/EgDY4pRh1cdCrrL7sO5sxnQBe-aWRT8KO9MFDA-3-x2ETg?e=1eTL8m",
+          },
+		  {
+            topic: "Lesson03 認識LED閃爍電路"
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EshCxudHBVdDuHaimyeds8ABBu4FmWBXHPiRjYAJg3PrSw?e=N1bcuC",
+          },
+		  {
+            topic: "Lesson04 如何選擇電池？"
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/Evc3Qa8hpd9JsKE_7FFFlAEBYbsUwm9jQ8MGyq--pRxzIw?e=kXD1y0",
+          },
+		  {
+            topic: "Lesson05 電子復活蛋"
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EszJgLxwUFtNh0xwV7Ns2HkBp49usNgtbc3EqsdzUNanCw?e=VMLFU7",
+          },
+		  {
+            topic: "Lesson06 設計無綫LED"
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/Eng6dkrqdzVJklD6A8--CA0BPX8l5kKkZuNB_MyPEdDExg?e=4kxhHA",
           },
         ],
       },
       {
-        courseName: "Advanced to PCB",
-        courseID: "pcb002",
+        courseName: "基礎電子與電路設計",
+        courseID: "PCB002",
         description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum molestiae delectus culpa hic assumenda, voluptate reprehenderit dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit, eveniet ex deserunt fuga?",
-        category: "hardware",
+          "世上電子元件多不勝數，如何選適合的元件由理解原理及閱讀數據手册開始。透過分析和建立各種電路，成為精明的微電子工程師！",
+        category: "Hardware",
         image: "",
         resources: [
           {
@@ -47,11 +67,11 @@ export const CourseInfo = [
         ],
       },
       {
-        courseName: "Master to PCB",
+        courseName: "工具/儀器/安全護具教學",
         courseID: "pcb003",
         description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum molestiae delectus culpa hic assumenda, voluptate reprehenderit dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit, eveniet ex deserunt fuga?",
-        category: "hardware",
+          "工欲善其事，必先利其器，安全第一。為工程師做任何工作時的兩個首要考慮。如何選正確的工具及用安全方式操作，會在這單元一目了然！",
+        category: "Hardware",
         image: "",
         resources: [
           {
