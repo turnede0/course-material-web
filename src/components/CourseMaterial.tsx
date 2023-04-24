e07dfd8a6aa8ce67230edef9b002a52b85ba0014
alexlam184 <alexwork0211@gmail.com>
2023-04-24 22:25:42 +0800

    fix : solve buuton problem

--- src/components/CourseMaterial.tsx
+++ src/components/CourseMaterial.tsx
@@ -48,18 +48,14 @@ const CourseMaterial = ({
                       {item.topic}
                     </td>
                     <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      <button
-                        type="button"
-                        className="inline-block px-6 py-2.5 bg-[#00df9a] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-black hover:text-[#00df9a] hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                      >
-                        <a
-                          href={item.downloadUrl}
-                          rel="noreferrer"
+                      <a href={item.downloadUrl} rel="noreferrer" type="button">
+                        <button
                           type="button"
+                          className="inline-block px-6 py-2.5 bg-[#00df9a] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-black hover:text-[#00df9a] hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                         >
                           Download
-                        </a>
-                      </button>
+                        </button>
+                      </a>
                     </td>
                   </tr>
                 ))}
