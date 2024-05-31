3293c8881709a2b748450ad9c31552649f419ccb
turnede0 <79206052+turnede0@users.noreply.github.com>
2024-05-31 15:08:16 +0800

    update

--- src/assets/course/CourseCategory.ts
+++ src/assets/course/CourseCategory.ts
@@ -12,28 +12,28 @@ export const CourseCategory: CourseCategoryType = [
     categoryID: "PCB Competition",
     categoryName: "PCB Competition",
     imgUrl:
-      "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
+      "https://i.imgur.com/V877Rpn.png",
     isActive: true,
   },
   {
     categoryID: "workshop",
     categoryName: "Workshop",
     imgUrl:
-      "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp",
+      "https://i.imgur.com/gyWBzU5.png",
     isActive: true,
   },
   {
     categoryID: "software_Driver",
     categoryName: "Software & Driver",
     imgUrl:
-      "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp",
+      "https://i.imgur.com/VG5hTlL.png",
     isActive: true,
   },
   {
     categoryID: "fpga",
     categoryName: "FPGA",
     imgUrl:
-      "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(78).webp",
+      "https://i.imgur.com/tmj4q4Q.png",
     isActive: true,
   },
   {
