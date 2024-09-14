f08204b77d87efc7ca4294e47f72ab1b49aae513
turnede0 <79206052+turnede0@users.noreply.github.com>
2024-09-14 20:24:33 +0800

    alex

--- src/assets/course/CourseInfo.ts
+++ src/assets/course/CourseInfo.ts
@@ -761,6 +761,17 @@ export const CourseInfo: CourseInfoType = [
           },
         ],
       },
+      {
+        courseName: "香港電子科技商會 - IFF 2024 微電子主題工作坊",
+        courseID: "HKETAIFF2024",
+        description: "香港電子科技商會 - IFF 2024 微電子主題工作坊",
+        categoryID: "workshop",
+        image:
+          "https://th.bing.com/th/id/R.bfa21ec379f7c009c1ca3262a0727684?rik=G0Dwv0FU19z%2fjg&pid=ImgRaw&r=0",
+        downloadUrl:
+          "https://stemturnede.sharepoint.com/:f:/g/ErtigeSI-FxAkfe1iU_jZEABsd88VKqn7lCZBKA7IiugJw?e=kDOtaJ",
+        
+      },
     ],
   },
   {
