6e663d753263111fe1afd8d5734643e9aa664997
turnede0 <79206052+turnede0@users.noreply.github.com>
2024-11-22 11:21:45 +0800

    updated

--- src/assets/course/CourseInfo.ts
+++ src/assets/course/CourseInfo.ts
@@ -1,216 +1,6 @@
 import type { CourseInfoType } from "../../utils/types";
 export const CourseInfo: CourseInfoType = [
-  {
-    categoryID: "hardware",
-    categoryDescription:
-      "無論是顯示器，平板電腦還是智能手機，都是硬件。 我們社會所有現有技術的皆由硬件支撐。 通過物理和電子知識的各種組合，我們可以創造出從日常生活、科學研究、能量采集到硬件加速等任何方面來幫助人類的設備",
-    courses: [
-      {
-        courseName: "印刷電路板入門",
-        courseID: "PCB001",
-        description:
-          "從零開始的印刷電路板設計入門之旅！手把手教程學習如何設計原理圖及佈板。制造属於您的第一塊電板！",
-        categoryID: "hardware",
-        image:
-          "https://i.imgur.com/g6zJVtg_d.webp?maxwidth=1520&fidelity=grand",
-        downloadUrl:
-          "https://stemturnede.sharepoint.com/sites/info.public.turned-e/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2Finfo%2Epublic%2Eturned-e%2FShared%20Documents%2FCourse&p=true&ga=1",
-        resources: [
-          {
-            topic: "Lesson01 認識EasyEDA",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:b:/g/EUX6RkqsbTNJh0jcvj3IJLMBF-cIp_6MVa3ZavJYlk-1Ag?e=8fG1I0",
-          },
-          {
-            topic: "Lesson02 電路設計101",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EgDY4pRh1cdCrrL7sO5sxnQBe-aWRT8KO9MFDA-3-x2ETg?e=1eTL8m",
-          },
-          {
-            topic: "Lesson03 認識LED閃爍電路",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EshCxudHBVdDuHaimyeds8ABBu4FmWBXHPiRjYAJg3PrSw?e=N1bcuC",
-          },
-          {
-            topic: "Lesson04 如何選擇電池？",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/Evc3Qa8hpd9JsKE_7FFFlAEBYbsUwm9jQ8MGyq--pRxzIw?e=kXD1y0",
-          },
-          {
-            topic: "Lesson05 電子復活蛋",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EszJgLxwUFtNh0xwV7Ns2HkBp49usNgtbc3EqsdzUNanCw?e=VMLFU7",
-          },
-          {
-            topic: "Lesson06 設計無綫LED",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/Eng6dkrqdzVJklD6A8--CA0BPX8l5kKkZuNB_MyPEdDExg?e=4kxhHA",
-          },
-          {
-            topic: "Lesson 07 如何製作MP3播放器",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EoVn7l6LrOVAv3EUX3dy9NcBtIY6rwR2VfmoIv0u0z-aRw?e=z0fRZw",
-          },
-          {
-            topic: "Lesson 08 如何燒錄語音芯片",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/El2hb725KOJMvCLvhDCiYMEBa9keUEbozXVP_1TuEPQdIQ?e=fCnpIu",
-          },
-        ],
-      },
-      {
-        courseName: "基礎電子與電路設計",
-        courseID: "PCB002",
-        description:
-          "世上電子元件多不勝數，如何選適合的元件由理解原理及閱讀數據手册開始。透過分析和建立各種電路，成為精明的微電子工程師！",
-        categoryID: "hardware",
-        image:
-          "https://i.imgur.com/bHnry7v_d.webp?maxwidth=1520&fidelity=grand",
-        resources: [
-          {
-            topic: "SMT焊接練習",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EnRHUHQZGMBAgdQyuR8sQm0BE-IjqEVWt5AaXrSVje3dgw?e=3Q8Bni0",
-          },
-          {
-            topic: "脈寬調變與伺服電機",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/Eoo-jzOrvcVEuCnweKYAM10BAkErcDF7wpEJ_b-yQCI01g?e=yc1OBQ",
-          },
-          {
-            topic: "PCB作品：個人名片",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EpYEVV0EzS9Fu4r9zwxiQiIBTjfWvC2VyWsjf2agD5Rj3Q?e=Yn6qBL",
-          },
-          {
-            topic: "MP3 播放器",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EtSd0UZPy_tDpmgLMdN8iBQBBUPki_CrC3wanFMl-5ajLQ?e=Q43VE6",
-          },
-          {
-            topic: "NFC 個人名片焊接和設置",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EjHiL4uVIQBEolvhdWWeEeABRfBs_8NiwXwgN5mVWS6_-g?e=sEHNzc",
-          },
-          {
-            topic: "ESP32",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EvgA_tMyWDFJkuQJsW9XkgQBISdGukKLBvT-yPqL0GIGWw?e=vQWWMr",
-          },
-          {
-            topic: "MQTT與傳感器",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EvoHgTfx765KsqEm6R_kgG0Bo3NuUcaoSz1mKOuJM0FQkA?e=nkwidR",
-          },
-          {
-            topic: "ESP32 PCB繪製",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EmE9pMnPe_FFrJAu9WEkYWgBGKE1uR1B150Dyw7hw2mvSQ?e=ezmhP2",
-          },
-          {
-            topic: "數據可視化",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/ElD3X2CUk9FMqDSQ9LkMi4gBawyISnorIcuWmD9SccH2FQ?e=ArZrX6",
-          },
-        ],
-      },
-      {
-        courseName: "工具/儀器/安全護具教學",
-        courseID: "PCB003",
-        description:
-          "工欲善其事，必先利其器，安全第一。為工程師做任何工作時的兩個首要考慮。如何選正確的工具及用安全方式操作，會在這單元一目了然！",
-        categoryID: "hardware",
-        image:
-          "https://i.imgur.com/FXxenv1_d.webp?maxwidth=1520&fidelity=grand",
-        resources: [],
-      },
-
-      {
-        courseName: "PCB&AI",
-        courseID: "PCB004",
-        description:
-          "設計自己的電路板去整合傳感器AI芯片,通訊模組,並製作3D外殼以建立屬於自己的語音無線遙控器",
-        categoryID: "hardware",
-        image:
-          "https://i.imgur.com/RXXtKo4.png",
-        downloadUrl:
-          "https://stemturnede.sharepoint.com/sites/info.public.turned-e/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2Finfo%2Epublic%2Eturned-e%2FShared%20Documents%2FCourse&p=true&ga=1",
-        resources: [
-          {
-            topic: "Lesson01 PCB電路設計",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/Ek-Okwi_ioNDn80JFuMwm1YB9uX_hHJ1hQejkuBz4ZMS9A?e=KfsEXe",
-          },
-          {
-            topic: "Lesson02 PCB布版設計",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EtVNzsOQmM1OtstEbn__afEBi17YB0J94BqEZzsP87SWog?e=QVJJxq",
-          },
-          {
-            topic: "Lesson03 語音AI",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/Engs5lHUN8ZDk0gVbkrehNEBhZMI01qXtIZRiowaaIQ5gg?e=fAzd93",
-          },
-          {
-            topic: "Lesson04 設計思維與3D設計",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EuaQRDpVr_hHntqqamM9NVIBcE-pUXEfssif4cmusAn-bA?e=hmXEif",
-          },
-          {
-            topic: "Lesson05 焊接，組裝，設計(1)",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EgAUlb9KbjZCuIBgLRhVbL0Bw4b5Hzcu0Z0GjZ1kUz2Bgw?e=yLvZAu",
-          },
-          {
-            topic: "Lesson06 焊接，組裝，設計(2)",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EiZ5uDOZHh1AksNWNQk95F8B6FvoP9C8TDxmS5zHpkepqg?e=xrRPqQ",
-          },
-          {
-            topic: "Lesson7 賽前測試",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/EuKPVazogatNiTk_ceZSwrABQ8xKY8AVJmNQXd0V5Jm68Q?e=xieZHR",
-          },
-          {
-            topic: "Lesson8 比賽環節",
-            downloadUrl:
-              "https://stemturnede.sharepoint.com/:f:/g/Et1kP4cV25xIhhZDtDcY7yEBqTCqAC3g-3PfF3fBeaHkQA?e=rvLfUv",
-          },
-        ]
-      },
-      {
-        courseName: "NFC個人卡片,設計及焊接",
-        courseID: "PCB005",
-        description:
-          "由電子入門到設計自己的NFC卡片,用生動有趣的方式初次接觸微電子",
-        categoryID: "hardware",
-        image:
-          "https://i.imgur.com/CNSWNHa.png",
-        resources: [          
-        {
-          topic: " NFC卡片設計",
-          downloadUrl:
-            "https://stemturnede.sharepoint.com/:f:/g/EpL77KQSRaRMlLigd8kmAw4Bc9KvzYhlg6aslj1NoRNUpg?e=Bbn0xu",
-        },],
-      },
-      {
-        courseName: "MP3播放器",
-        courseID: "PCB006",
-        description:
-          "由電子入門到設計自己的MP3播放器,用生動有趣的方式初次接觸微電子",
-        categoryID: "hardware",
-        image:
-          "https://i.imgur.com/CNSWNHa.png",
-        resources: [          
-        {
-          topic: "MP3播放器",
-          downloadUrl:
-            "https://stemturnede.sharepoint.com/:f:/g/ErmQ3e-njB1Eueg8g7560xcBJyIUbYLn-OrVr562PwqoGg?e=dOQddM",
-        },],
-      },
-
-    ],
-  },
+  
   {
     categoryID: "omron",
     categoryDescription: "雪花燈PCB焊接X機械人建設體驗",
@@ -516,7 +306,29 @@ export const CourseInfo: CourseInfoType = [
       ],
         
       },
-
+      {
+        courseName: "Microbit X PCB 簡易入門課程",
+        courseID: "PCB01.12",
+        description:
+          "通過15小時的課程，同學學習使用micro:bit， 由0開始製作智能家居語音溫濕度傳感器專題。",
+        categoryID: "PCB",
+        image:
+          "https://i.imgur.com/CNSWNHa.png",
+        resources: [          
+        {
+          topic: " Lesson 1-3 Microbit 智能家居語音溫濕度傳感器",
+          downloadUrl:
+            "https://stemturnede.sharepoint.com/:f:/g/EtjveCJa4m9Jo2ivs7olQnMBmuwkPQlhMZnv86RfsejOWw?e=9d82pD",
+        },
+        {
+          topic: " Lesson 4-9 PCB設計",
+          downloadUrl:
+            "https://stemturnede.sharepoint.com/:f:/g/EsaP1zwOcOdEhyfQc1oov9kBxlMmm5PNyp1QdcGJ_NVRnQ?e=XhjE4m",
+        },
+      
+      ],
+        
+      },
 
       {
         courseName: "電子工程基礎課程",
@@ -1012,6 +824,217 @@ export const CourseInfo: CourseInfoType = [
           },
         ]
       }
+    ],
+  },
+  {
+    categoryID: "hardware",
+    categoryDescription:
+      "無論是顯示器，平板電腦還是智能手機，都是硬件。 我們社會所有現有技術的皆由硬件支撐。 通過物理和電子知識的各種組合，我們可以創造出從日常生活、科學研究、能量采集到硬件加速等任何方面來幫助人類的設備",
+    courses: [
+      {
+        courseName: "印刷電路板入門",
+        courseID: "PCB001",
+        description:
+          "從零開始的印刷電路板設計入門之旅！手把手教程學習如何設計原理圖及佈板。制造属於您的第一塊電板！",
+        categoryID: "hardware",
+        image:
+          "https://i.imgur.com/g6zJVtg_d.webp?maxwidth=1520&fidelity=grand",
+        downloadUrl:
+          "https://stemturnede.sharepoint.com/sites/info.public.turned-e/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2Finfo%2Epublic%2Eturned-e%2FShared%20Documents%2FCourse&p=true&ga=1",
+        resources: [
+          {
+            topic: "Lesson01 認識EasyEDA",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:b:/g/EUX6RkqsbTNJh0jcvj3IJLMBF-cIp_6MVa3ZavJYlk-1Ag?e=8fG1I0",
+          },
+          {
+            topic: "Lesson02 電路設計101",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EgDY4pRh1cdCrrL7sO5sxnQBe-aWRT8KO9MFDA-3-x2ETg?e=1eTL8m",
+          },
+          {
+            topic: "Lesson03 認識LED閃爍電路",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EshCxudHBVdDuHaimyeds8ABBu4FmWBXHPiRjYAJg3PrSw?e=N1bcuC",
+          },
+          {
+            topic: "Lesson04 如何選擇電池？",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/Evc3Qa8hpd9JsKE_7FFFlAEBYbsUwm9jQ8MGyq--pRxzIw?e=kXD1y0",
+          },
+          {
+            topic: "Lesson05 電子復活蛋",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EszJgLxwUFtNh0xwV7Ns2HkBp49usNgtbc3EqsdzUNanCw?e=VMLFU7",
+          },
+          {
+            topic: "Lesson06 設計無綫LED",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/Eng6dkrqdzVJklD6A8--CA0BPX8l5kKkZuNB_MyPEdDExg?e=4kxhHA",
+          },
+          {
+            topic: "Lesson 07 如何製作MP3播放器",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EoVn7l6LrOVAv3EUX3dy9NcBtIY6rwR2VfmoIv0u0z-aRw?e=z0fRZw",
+          },
+          {
+            topic: "Lesson 08 如何燒錄語音芯片",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/El2hb725KOJMvCLvhDCiYMEBa9keUEbozXVP_1TuEPQdIQ?e=fCnpIu",
+          },
+        ],
+      },
+      {
+        courseName: "基礎電子與電路設計",
+        courseID: "PCB002",
+        description:
+          "世上電子元件多不勝數，如何選適合的元件由理解原理及閱讀數據手册開始。透過分析和建立各種電路，成為精明的微電子工程師！",
+        categoryID: "hardware",
+        image:
+          "https://i.imgur.com/bHnry7v_d.webp?maxwidth=1520&fidelity=grand",
+        resources: [
+          {
+            topic: "SMT焊接練習",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EnRHUHQZGMBAgdQyuR8sQm0BE-IjqEVWt5AaXrSVje3dgw?e=3Q8Bni0",
+          },
+          {
+            topic: "脈寬調變與伺服電機",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/Eoo-jzOrvcVEuCnweKYAM10BAkErcDF7wpEJ_b-yQCI01g?e=yc1OBQ",
+          },
+          {
+            topic: "PCB作品：個人名片",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EpYEVV0EzS9Fu4r9zwxiQiIBTjfWvC2VyWsjf2agD5Rj3Q?e=Yn6qBL",
+          },
+          {
+            topic: "MP3 播放器",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EtSd0UZPy_tDpmgLMdN8iBQBBUPki_CrC3wanFMl-5ajLQ?e=Q43VE6",
+          },
+          {
+            topic: "NFC 個人名片焊接和設置",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EjHiL4uVIQBEolvhdWWeEeABRfBs_8NiwXwgN5mVWS6_-g?e=sEHNzc",
+          },
+          {
+            topic: "ESP32",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EvgA_tMyWDFJkuQJsW9XkgQBISdGukKLBvT-yPqL0GIGWw?e=vQWWMr",
+          },
+          {
+            topic: "MQTT與傳感器",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EvoHgTfx765KsqEm6R_kgG0Bo3NuUcaoSz1mKOuJM0FQkA?e=nkwidR",
+          },
+          {
+            topic: "ESP32 PCB繪製",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EmE9pMnPe_FFrJAu9WEkYWgBGKE1uR1B150Dyw7hw2mvSQ?e=ezmhP2",
+          },
+          {
+            topic: "數據可視化",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/ElD3X2CUk9FMqDSQ9LkMi4gBawyISnorIcuWmD9SccH2FQ?e=ArZrX6",
+          },
+        ],
+      },
+      {
+        courseName: "工具/儀器/安全護具教學",
+        courseID: "PCB003",
+        description:
+          "工欲善其事，必先利其器，安全第一。為工程師做任何工作時的兩個首要考慮。如何選正確的工具及用安全方式操作，會在這單元一目了然！",
+        categoryID: "hardware",
+        image:
+          "https://i.imgur.com/FXxenv1_d.webp?maxwidth=1520&fidelity=grand",
+        resources: [],
+      },
+
+      {
+        courseName: "PCB&AI",
+        courseID: "PCB004",
+        description:
+          "設計自己的電路板去整合傳感器AI芯片,通訊模組,並製作3D外殼以建立屬於自己的語音無線遙控器",
+        categoryID: "hardware",
+        image:
+          "https://i.imgur.com/RXXtKo4.png",
+        downloadUrl:
+          "https://stemturnede.sharepoint.com/sites/info.public.turned-e/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2Finfo%2Epublic%2Eturned-e%2FShared%20Documents%2FCourse&p=true&ga=1",
+        resources: [
+          {
+            topic: "Lesson01 PCB電路設計",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/Ek-Okwi_ioNDn80JFuMwm1YB9uX_hHJ1hQejkuBz4ZMS9A?e=KfsEXe",
+          },
+          {
+            topic: "Lesson02 PCB布版設計",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EtVNzsOQmM1OtstEbn__afEBi17YB0J94BqEZzsP87SWog?e=QVJJxq",
+          },
+          {
+            topic: "Lesson03 語音AI",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/Engs5lHUN8ZDk0gVbkrehNEBhZMI01qXtIZRiowaaIQ5gg?e=fAzd93",
+          },
+          {
+            topic: "Lesson04 設計思維與3D設計",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EuaQRDpVr_hHntqqamM9NVIBcE-pUXEfssif4cmusAn-bA?e=hmXEif",
+          },
+          {
+            topic: "Lesson05 焊接，組裝，設計(1)",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EgAUlb9KbjZCuIBgLRhVbL0Bw4b5Hzcu0Z0GjZ1kUz2Bgw?e=yLvZAu",
+          },
+          {
+            topic: "Lesson06 焊接，組裝，設計(2)",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EiZ5uDOZHh1AksNWNQk95F8B6FvoP9C8TDxmS5zHpkepqg?e=xrRPqQ",
+          },
+          {
+            topic: "Lesson7 賽前測試",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/EuKPVazogatNiTk_ceZSwrABQ8xKY8AVJmNQXd0V5Jm68Q?e=xieZHR",
+          },
+          {
+            topic: "Lesson8 比賽環節",
+            downloadUrl:
+              "https://stemturnede.sharepoint.com/:f:/g/Et1kP4cV25xIhhZDtDcY7yEBqTCqAC3g-3PfF3fBeaHkQA?e=rvLfUv",
+          },
+        ]
+      },
+      {
+        courseName: "NFC個人卡片,設計及焊接",
+        courseID: "PCB005",
+        description:
+          "由電子入門到設計自己的NFC卡片,用生動有趣的方式初次接觸微電子",
+        categoryID: "hardware",
+        image:
+          "https://i.imgur.com/CNSWNHa.png",
+        resources: [          
+        {
+          topic: " NFC卡片設計",
+          downloadUrl:
+            "https://stemturnede.sharepoint.com/:f:/g/EpL77KQSRaRMlLigd8kmAw4Bc9KvzYhlg6aslj1NoRNUpg?e=Bbn0xu",
+        },],
+      },
+      {
+        courseName: "MP3播放器",
+        courseID: "PCB006",
+        description:
+          "由電子入門到設計自己的MP3播放器,用生動有趣的方式初次接觸微電子",
+        categoryID: "hardware",
+        image:
+          "https://i.imgur.com/CNSWNHa.png",
+        resources: [          
+        {
+          topic: "MP3播放器",
+          downloadUrl:
+            "https://stemturnede.sharepoint.com/:f:/g/ErmQ3e-njB1Eueg8g7560xcBJyIUbYLn-OrVr562PwqoGg?e=dOQddM",
+        },],
+      },
+
     ],
   },
 ];
